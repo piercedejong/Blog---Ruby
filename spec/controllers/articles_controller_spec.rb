b3b7c67fe6e056fa71d50b88408b03165dc8151e
Pierce <pierce.dejong45@gmail.com>
2017-06-02 10:00:09 -0500

    Added testing for articles edit

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -5,9 +5,8 @@ RSpec.describe ArticlesController, type: :controller do
     email: "pierce@test.com",
     photo: "https://pbs.twimg.com/profile_images/856041767220649984/dJQxT0Dz.jpg")}
 
-
   let!(:art) {Article.create(title: "Test Post", text: "This is a test post",
-  about: "This is the about", user_id: current_user.id.to_s)}
+    about: "This is the about", user_id: current_user.id.to_s)}
 
   before do
     session[:user_id] = current_user.id
@@ -30,14 +29,32 @@ RSpec.describe ArticlesController, type: :controller do
 
   describe "PUT #update" do
     context "when editing an existing article" do
-      let :params do
-        {article: {title: "Test Post - Update", text: "This is a test post, UPDATE",
-        about: "This is the about, UPDATE", user_id: current_user.id.to_s}}
+      let(:attr) do { title: 'Test UPDATE', text: 'This is a test UPDATE',
+        about: "About UPDATE", user_id: current_user.id.to_s }
       end
-      it "update the selected article with the new aprams and redirects to article_path(param[:id])" do
-        put :update, id: art.id, article: params
-        expect(art).to eq params[:article]
-        expect(response).to redirect_to article_path(art.id)
+    before(:each) do
+      put :update, id: art.id, article: attr
+      art.reload
+    end
+    it "update the selected article with the new aprams and redirects to article_path(param[:id])" do
+      expect(art.title).to eq attr[:title]
+      expect(art.about).to eq attr[:about]
+      expect(art.text).to eq attr[:text]
+      expect(response).to redirect_to article_path(art.id)
+      end
+    end
+
+    context "Editing an exisitng article with the incorrect params" do
+      let(:attr) do { title: 'DATE', text: 'This is a test UPDATE',
+        about: "About UPDATE", user_id: current_user.id.to_s }
+      end
+    before(:each) do
+      put :update, id: art.id, article: attr
+      art.reload
+    end
+    it "fail to updates seelcted article and redirect to current path" do
+      expect(art.title).to_not eq attr[:title]
+      expect(response).to_not redirect_to article_path(art.id)
       end
     end
   end
@@ -46,8 +63,6 @@ RSpec.describe ArticlesController, type: :controller do
     context "when deleting an article" do
       it "deletes the current article and redirects to artciles_path" do
         expect{delete :destroy,  {id: art.id}}.to change {Article.count}.by(-1)
-        #delete :destroy,  {id: art.id}
-        #binding.pry
         expect(response).to redirect_to articles_path
       end
     end
